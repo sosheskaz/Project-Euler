ec005d66d586bead0845107da667028a47504597
Eric Miller <emiller@cleo.com>
2018-05-11 19:20:42 -0500

    49 done

--- Python/Euler049.py
+++ Python/Euler049.py
@@ -1,25 +1,30 @@
+#!/usr/bin/env python3
+import itertools
+import joblib
+import math
+import multiprocessing
 from sympy import sieve
 
 
 def main():
-    primes = [i for i in sieve.primerange(1000, 10000) if len(set(str(i))) == 4]
+    primes = list(sieve.primerange(1000, 10000))
+    combos = set()
+    ignore = {'148748178147'}
     # This is really inefficient, but it's a small enough data set that it works.
-    for prime in primes:
-        ps = str(prime)
-        matches = [ps]
-        for prime2 in [str(p) for p in primes if p > prime]:
-            has_match = True
-            for c in prime2:
-                if c not in ps:
-                    has_match = False
-                    break
-            if has_match:
-                matches.append(prime2)
-        if len(matches) >= 3:
-            for i, e1 in enumerate(matches[0:]):
-                for e2 in matches[i+1:]:
-                    if str(2 * int(e2) - int(e1)) in matches:
-                        print([e1, e2, str(2 * int(e2) - int(e1))])
+    # for prime in primes:
+    func = joblib.delayed(check_combos_for)
+    parallelism = multiprocessing.cpu_count()
+    combos.update(itertools.chain.from_iterable(joblib.Parallel(parallelism)(func(prime, primes) for prime in primes)))
+    combos.difference_update(ignore)
+    for combo in combos:
+        print(combo)
+
+def check_combos_for(prime, primelist):
+    cprime = set(str(prime))
+    matches = {p for p in primelist if p > prime and set(str(p)) == cprime}
+    return ['{}{}{}'.format(*trio)
+                        for trio in itertools.combinations(sorted(matches), 3)
+                        if trio[2] - trio[1] == trio[1] - trio[0]]
 
 
 if __name__ == '__main__':
