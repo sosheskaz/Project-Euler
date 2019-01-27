c5987a9a883fae9341bde79f7b10b56b7b7625e8
Eric Miller <eric.the.miller@icloud.com>
2019-01-26 22:13:32 -0600

    Added Powershell

--- multi.sh
+++ multi.sh
@@ -12,21 +12,24 @@ fname="Euler$number"
 
 gofile="Go/$fname.go"
 groovyfile="Groovy/$fname.groovy"
+pwshfile="Powershell/$fname.ps1"
 pythonfile="Python/$fname.py"
 rubyfile="Ruby/$fname.rb"
 swiftfile="Swift/$fname.swift"
 
-chmod +x "$groovyfile" "$pythonfile" "$rubyfile" "$swiftfile" > /dev/null 2>/dev/null
+chmod +x "$groovyfile" "$pythonfile" "$rubyfile" "$swiftfile" "$pwshfile" > /dev/null 2>/dev/null
 
 if [ -f "$gofile" ]; then goval=$(go run "$gofile" 2>/dev/null); fi
 if [ -f "$groovyfile" ]; then groovyval=$($groovyfile 2>/dev/null); fi
+if [ -f "$pwshfile" ]; then pwshval=$($pwshfile 2>/dev/null); fi
 if [ -f "$pythonfile" ]; then pythonval=$($pythonfile 2>/dev/null); fi
 if [ -f "$rubyfile" ]; then rubyval=$($rubyfile 2>/dev/null); fi
 if [ -f "$swiftfile" ]; then swiftval=$($swiftfile 2>/dev/null); fi
 
 echo "
-Go:     $goval
-Groovy: $groovyval
-Python: $pythonval
-Ruby:   $rubyval
-Swift:  $swiftval"
+Go:         $goval
+Groovy:     $groovyval
+Powershell: $pwshval
+Python:     $pythonval
+Ruby:       $rubyval
+Swift:      $swiftval"
