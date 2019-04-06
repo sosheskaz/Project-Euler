a33a6eb939117d4a68bacce4d690765e09b19735
Eric Miller <eric.the.miller@icloud.com>
2019-04-06 01:39:02 -0500

    Added divmod optimizations to Euler003

--- Python/Euler003.py
+++ Python/Euler003.py
@@ -1,23 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from __future__ import print_function
 
 
 def main():
-    print(list(prime_factors(600851475143))[-1])
+    print(max(prime_factors(600851475143)))
 
 
 def prime_factors(of_number):
-    while not (of_number % 2):
-        of_number /= 2
+    div, mod = divmod(of_number, 2)
+    while mod is 0:
+        of_number = div
         yield 2
+        div, mod = divmod(of_number, 2)
 
-    for i in xrange(3, of_number, 2):
-        if of_number == 1:
+    for i in range(3, of_number, 2):
+        if of_number is 1:
             break
 
-        while of_number % i == 0:
-            of_number /= i
+        div, mod = divmod(of_number, i)
+        while mod is 0:
+            of_number = div
             yield i
+            div, mod = divmod(of_number, i)
 
 
 if __name__ == '__main__':
--- Python/Euler004.py
+++ Python/Euler004.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from __future__ import print_function
 from itertools import combinations
 
 
 def main():
-    combos = combinations(xrange(100, 1000), 2)
+    combos = combinations(range(100, 1000), 2)
     product_strings = (str(x * y) for x, y in combos)
     palindromes = (int(s) for s in product_strings if s == s[::-1])
     biggest = max(palindromes)
