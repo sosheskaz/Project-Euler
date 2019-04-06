cd7d38eb47e9db9ed6c7c32104d8c8b92c1563e1
Eric Miller <eric.the.miller@icloud.com>
2019-04-06 01:21:24 -0500

    Re-did the implementation of 005 in python to use logarithms

--- Python/Euler005.py
+++ Python/Euler005.py
@@ -1,36 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from __future__ import print_function
+from functools import reduce
+from itertools import count, takewhile
+from math import log
 from operator import mul
-from itertools import groupby
+# from sympy import sieve
 
 
 def main():
-    factorizations = (prime_factors(i) for i in xrange(2, 21))
-    frequencies = {}
-    for factorization in factorizations:
-        tmp_frequencies = {prime: len(list(freq)) for prime, freq in groupby(factorization)}
-        to_overwrite = {key: value
-                        for key, value in tmp_frequencies.items()
-                        if key not in frequencies or value > frequencies[key]}
-        frequencies.update(to_overwrite)
-
-    sub_products = (factor ** freq for factor, freq in frequencies.items())
-    product = reduce(mul, sub_products)
-    print(product)
+    # Sympy is concise and efficient, but the front-loading cost is much greater
+    # than the performance increase for n = 20.
+    # primes = sieve.primerange(0, 20)
+    primes = takewhile(lambda x: x < 20, get_primes())
+    prime_pow = lambda prime, ceiling: prime ** int(log(ceiling, prime))
+    result = reduce(mul, (prime_pow(prime, 20) for prime in primes))
+    print(result)
 
 
-def prime_factors(of_number):
-    while not (of_number % 2):
-        of_number /= 2
-        yield 2
-
-    for i in xrange(3, of_number + 1, 2):
-        if of_number == 1:
-            break
-
-        while of_number % i == 0:
-            of_number /= i
+def get_primes():
+    yield 2
+    prime_checker = []
+    for i in count(3, 2):
+        if all(i % prev_prime for prev_prime in prime_checker):
             yield i
+            prime_checker.append(i)
 
 
 if __name__ == '__main__':
